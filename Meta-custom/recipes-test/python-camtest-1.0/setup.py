03be08d55b813c4948e382e96adcd572533109be
Jefryruiz <67134363+Jefryruiz@users.noreply.github.com>
2020-06-29 02:03:53 -0500

    Update setup.py

--- Meta-custom/recipes-test/python-camtest-1.0/setup.py
+++ Meta-custom/recipes-test/python-camtest-1.0/setup.py
@@ -1,1 +1,13 @@
-
+import sys
+from setuptools import setup
+setup(
+    name = "camtest",
+    version = "0.1",
+    packages=["camtest"],
+    author="Alex Gonzalez",
+    author_email = "alex@example.com",
+    description = "Hello World packaging example",
+    license = "MIT",
+    keywords= "example",
+    url = "",
+)
